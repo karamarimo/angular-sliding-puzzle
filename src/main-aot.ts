f0379061fe67bfa26a04e9fe14bcf53b67a8b714
karamarimo <kubotaryou.ryoukubota@gmail.com>
2017-04-06 20:45:17 +0900

    change main-aot.ts to run in product mode if not run in localhost

--- src/main-aot.ts
+++ src/main-aot.ts
@@ -1,5 +1,9 @@
 import { platformBrowser }    from '@angular/platform-browser';
 import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
+import { enableProdMode } from '@angular/core';
 
-console.log('Running AOT compiled');
+// Enable production mode unless running locally
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
